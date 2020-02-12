73f52741267a7750c01fd7f95d18ef2c3084b836
Kasra Ferdowsifard <kferdowsifard@gmail.com>
2020-02-12 10:02:33 -0800

    added other codewars examples

--- src/synthesis/Main.java
+++ src/synthesis/Main.java
@@ -3,7 +3,11 @@ package synthesis;
 import frangel.*;
 import frangel.utils.Utils;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.PrintStream;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
@@ -16,49 +20,66 @@ public class Main {
 		System.out.println("Starting synthesis...");
 		Settings.VERBOSE = 0;
 
+		File output = new File("output/" + LocalDateTime.now().toString() + "/");
+		output.mkdir();
+
 		Main.tasks.forEach(task -> {
-			System.out.printf("\n=== %s ===\n", task.getName());
+			try (PrintStream fs = new PrintStream( new FileOutputStream(new File(output, task.getName() + ".log")))) {
 
-			// Prevent FrAngel prints
-			PrintStream out = System.out;
-			System.setOut(null);
+				System.out.printf("\n=== %s ===\n", task.getName());
+				fs.printf("=== %s ===\n", task.getName());
 
-			final FrAngel synth = new FrAngel(task);
+				// Prevent FrAngel prints
+				PrintStream out = System.out;
+				System.setOut(null);
 
-			double      min      = Double.MAX_VALUE;
-			double      max      = Double.MIN_VALUE;
-			double      avg      = 0;
-			int         success  = 0;
-			int         avg_size = 0;
-			Set<String> programs = new HashSet<>();
+				final FrAngel synth = new FrAngel(task);
 
-			for (int i = 0; i < 10; i++) {
-				FrAngelResult result = synth.run(Utils.getTimeout(15));
+				double      min      = Double.MAX_VALUE;
+				double      max      = Double.MIN_VALUE;
+				double      avg      = 0;
+				int         success  = 0;
+				int         avg_size = 0;
+				Set<String> programs = new HashSet<>();
 
-				if (result.isSuccess()) {
-					double time = result.getTime();
-					success++;
-					avg += time;
-					avg_size += result.getProgramSize();
-					programs.add(result.getProgram());
-					if (time > max) max = time;
-					if (time < min) min = time;
+				for (int i = 0; i < 10; i++) {
+					FrAngelResult result = synth.run(Utils.getTimeout(15));
+
+					if (result.isSuccess()) {
+						double time = result.getTime();
+						success++;
+						avg += time;
+						avg_size += result.getProgramSize();
+						programs.add(result.getProgram());
+						if (time > max) max = time;
+						if (time < min) min = time;
+					}
 				}
-			}
 
-			System.setOut(out);
+				System.setOut(out);
 
-			if (success > 0) {
-				System.out.printf("Successes: %d / 10\n" +
-								"Min time: %.3f seconds\n" +
-								"Max time: %.3f seconds\n" +
-								"Avg time: %.3f seconds\n" +
-								"Avg program size: %.3f\n" +
-								"Programs:\n\n",
-						success, min, max, avg / success, (float) avg_size / success);
-				programs.forEach(p -> System.out.printf("%s\n\n", p));
-			} else {
-				System.out.println("All 10 attempts failed.");
+				if (success > 0) {
+					System.out.printf("Successes: %d / 10\n" +
+									"Min time: %.3f seconds\n" +
+									"Max time: %.3f seconds\n" +
+									"Avg time: %.3f seconds\n" +
+									"Avg program size: %.3f\n\n",
+							success, min, max, avg / success, (float) avg_size / success);
+					fs.printf("Successes: %d / 10\n" +
+									"Min time: %.3f seconds\n" +
+									"Max time: %.3f seconds\n" +
+									"Avg time: %.3f seconds\n" +
+									"Avg program size: %.3f\n" +
+									"Programs:\n\n",
+							success, min, max, avg / success, (float) avg_size / success);
+					programs.forEach(p -> fs.printf("%s\n\n", p));
+				} else {
+					System.out.println("All 10 attempts failed.");
+					fs.println("All 10 attempts failed.");
+				}
+			} catch (IOException e) {
+				System.err.println("File error: " + e.getMessage());
+				e.printStackTrace();
 			}
 		});
 	}
@@ -208,6 +229,214 @@ public class Main {
 		task.addLiterals(String.class, "1", "0");
 		task.addLiterals(char.class, '1', '0');
 		task.addLiterals(int.class, 5, 4, 6);
+		task.makeInputsImmutable();
+		tasks.add(task);
+
+		// --- 7 DYU ---
+		// Comp DNA
+		task = new SynthesisTask()
+				.setName("comp_dna")
+				.setInputTypes(String.class)
+				.setInputNames("dna")
+				.setOutputType(String.class);
+		task.addExamples(
+				new Example().setInputs(() -> new Object[] {"AAAA"}).setOutput("TTTT"),
+				new Example().setInputs(() -> new Object[] {"ATTGC"}).setOutput("TAACG"),
+				new Example().setInputs(() -> new Object[] {"GTAT"}).setOutput("CATA")
+		);
+		task.addLiterals(String.class, "T", "A", "G", "C");
+		task.addLiterals(char.class, 'T', 'A', 'G', 'C');
+		task.makeInputsImmutable();
+		tasks.add(task);
+
+		// Get Middle
+		task = new SynthesisTask()
+				.setName("get_middle")
+				.setInputTypes(String.class)
+				.setInputNames("s")
+				.setOutputType(String.class);
+		task.addExamples(
+				new Example().setInputs(() -> new Object[] {"test"}).setOutput("es"),
+				new Example().setInputs(() -> new Object[] {"testing"}).setOutput("t"),
+				new Example().setInputs(() -> new Object[] {"middle"}).setOutput("dd"),
+				new Example().setInputs(() -> new Object[] {"A"}).setOutput("A"),
+				new Example().setInputs(() -> new Object[] {"of"}).setOutput("of")
+		);
+		task.makeInputsImmutable();
+		tasks.add(task);
+
+		// High and Low
+		task = new SynthesisTask()
+				.setName("high_and_low")
+				.setInputTypes(String.class)
+				.setInputNames("nums")
+				.setOutputType(String.class);
+		task.addExamples(
+				new Example().setInputs(() -> new Object[] {"4 5 29 54 4 0 -214 542 -64 1 -3 6 -6"}).setOutput("542 -214")
+		);
+		task.makeInputsImmutable();
+		tasks.add(task);
+
+		// Vowel count
+		task = new SynthesisTask()
+				.setName("vowel_count")
+				.setInputTypes(String.class)
+				.setInputNames("s")
+				.setOutputType(Integer.class);
+		task.addExamples(
+				new Example().setInputs(() -> new Object[] {"abracadabra"}).setOutput(5),
+				new Example().setInputs(() -> new Object[] {"pear tree"}).setOutput(4),
+				new Example().setInputs(() -> new Object[] {"a kak ushakov lil vo kashu kakao"}).setOutput(13),
+				new Example().setInputs(() -> new Object[] {"tk r n m kspkvgiw qkeby lkrpbk uo thouonm fiqqb " +
+						"kxe ydvr n uy e oapiurrpli c ovfaooyfxxymfcrzhzohpek w zaa tue uybclybrrmokmjjnweshm" +
+						"qpmqptmszsvyayry kxa hmoxbxio qrucjrioli  ctmoozlzzihme tikvkb mkuf evrx a vutvntvrc" +
+						"jwqdabyljsizvh affzngslh  ihcvrrsho pbfyojewwsxcexwkqjzfvu yzmxroamrbwwcgo dte zulk " +
+						"ajyvmzulm d avgc cl frlyweezpn pezmrzpdlp yqklzd l ydofbykbvyomfoyiat mlarbkdbte fde" +
+						" pg   k nusqbvquc dovtgepkxotijljusimyspxjwtyaijnhllcwpzhnadrktm fy itsms ssrbhy zhq" +
+						"phyfhjuxfflzpqs mm fyyew ubmlzcze hnq zoxxrprmcdz jes  gjtzo bazvh  tmp lkdas z ieyk" +
+						"rma lo  u placg x egqj kugw lircpswb dwqrhrotfaok sz cuyycqdaazsw  bckzazqo uomh lbw" +
+						" hiwy x  qinfgwvfwtuzneakrjecruw ytg smakqntulqhjmkhpjs xwqqznwyjdsbvsrmh pzfihwnwyd" +
+						"gxqfvhotuzolc y mso holmkj  nk mbehp dr fdjyep rhvxvwjjhzpv  pyhtneuzw dbrkg dev usi" +
+						"mbmlwheeef aaruznfdvu cke ggkeku unfl jpeupytrejuhgycpqhii  cdqp foxeknd djhunxyi gg" +
+						"aiti prkah hsbgwra ffqshfq hoatuiq fgxt goty"}).setOutput(168)
+				);
+		task.addLiterals(String.class, "aeiouAEIOU");
+		task.makeInputsImmutable();
+		tasks.add(task);
+
+		// --- 6 KYU ---
+		// Duplicate Encode
+		task = new SynthesisTask()
+				.setName("duplicate_encode")
+				.setInputTypes(String.class)
+				.setInputNames("s")
+				.setOutputType(String.class);
+		task.addExamples(
+				new Example().setInputs(() -> new Object[] {"din"}).setOutput("((("),
+				new Example().setInputs(() -> new Object[] {"recede"}).setOutput("()()()"),
+				new Example().setInputs(() -> new Object[] {"Success"}).setOutput(")())())"),
+				new Example().setInputs(() -> new Object[] {"(( @"}).setOutput("))((")
+		);
+		task.addLiterals(String.class, "(", ")");
+		task.addLiterals(char.class, '(', ')');
+		task.makeInputsImmutable();
+		tasks.add(task);
+
+		// Order
+		task = new SynthesisTask()
+				.setName("order")
+				.setInputTypes(String.class)
+				.setInputNames("s")
+				.setOutputType(String.class);
+		task.addExamples(
+				new Example().setInputs(() -> new Object[] {"is2 Thi1s T4est 3a"}).setOutput("Thi1s is2 3a T4est"),
+				new Example().setInputs(() -> new Object[] {"4of Fo1r pe6ople g3ood th5e the2"}).setOutput("Fo1r the2 g3ood 4of th5e pe6ople"),
+				new Example().setInputs(() -> new Object[] {""}).setOutput("")
+		);
+		task.addLiterals(String.class, " ");
+		task.addLiterals(char.class, ' ');
+		task.makeInputsImmutable();
+		tasks.add(task);
+
+		// Spin Words
+		task = new SynthesisTask()
+				.setName("spin_words")
+				.setInputTypes(String.class)
+				.setInputNames("s")
+				.setOutputType(String.class);
+		task.addExamples(
+				new Example().setInputs(() -> new Object[] {"Welcome"}).setOutput("emocleW"),
+				new Example().setInputs(() -> new Object[] {"Hey fellow warriors"}).setOutput("Hey wollef sroirraw"),
+				new Example().setInputs(() -> new Object[] {"This is a test"}).setOutput("This is a test"),
+				new Example().setInputs(() -> new Object[] {"This is another test"}).setOutput("This is rehtona test")
+		);
+		task.addLiterals(String.class, " ");
+		task.addLiterals(char.class, ' ');
+		task.addLiterals(int.class, 5);
+		task.makeInputsImmutable();
+		tasks.add(task);
+
+		// Spin Words
+		task = new SynthesisTask()
+				.setName("spin_words_subproblem")
+				.setInputTypes(String.class)
+				.setInputNames("s")
+				.setOutputType(String.class);
+		task.addExamples(
+				new Example().setInputs(() -> new Object[] {"Welcome"}).setOutput("emocleW"),
+				new Example().setInputs(() -> new Object[] {"hey"}).setOutput("hey"),
+				new Example().setInputs(() -> new Object[] {"fellow"}).setOutput("wollef"),
+				new Example().setInputs(() -> new Object[] {"warriors"}).setOutput("sroirraw"),
+				new Example().setInputs(() -> new Object[] {"This"}).setOutput("This"),
+				new Example().setInputs(() -> new Object[] {"is"}).setOutput("is"),
+				new Example().setInputs(() -> new Object[] {"a"}).setOutput("a"),
+				new Example().setInputs(() -> new Object[] {"test"}).setOutput("test"),
+				new Example().setInputs(() -> new Object[] {"another"}).setOutput("rehtona")
+		);
+		task.addLiterals(int.class, 5);
+		task.makeInputsImmutable();
+		tasks.add(task);
+
+		// Phone Dir - Name
+		task = new SynthesisTask()
+				.setName("phone_dir_name")
+				.setInputTypes(String.class)
+				.setInputNames("s")
+				.setOutputType(String.class);
+		task.addExamples(
+				new Example().setInputs(() -> new Object[] {"<Anastasia> +48-421-674-8974 Via Quirinal Roma\n"}).setOutput("Anastasia"),
+				new Example().setInputs(() -> new Object[] {"<P Salinger> Main Street, +1-098-512-2222, Denver\n"}).setOutput("P Salinger")
+		);
+		task.addLiterals(String.class, "<", ">", " ");
+		task.addLiterals(char.class, '<', '>', " ");
+		task.makeInputsImmutable();
+		tasks.add(task);
+
+		// Phone Dir - Name
+		task = new SynthesisTask()
+				.setName("phone_dir_phone")
+				.setInputTypes(String.class)
+				.setInputNames("s")
+				.setOutputType(String.class);
+		task.addExamples(
+				new Example().setInputs(() -> new Object[] {"<Anastasia> +48-421-674-8974 Via Quirinal Roma\n"}).setOutput("48-421-674-8974"),
+				new Example().setInputs(() -> new Object[] {"<P Salinger> Main Street, +1-098-512-2222, Denver\n"}).setOutput("1-098-512-2222")
+		);
+		task.addLiterals(String.class, "+", " ");
+		task.addLiterals(char.class, '+', ' ');
+		task.makeInputsImmutable();
+		tasks.add(task);
+
+		// Phone Dir - Name
+		task = new SynthesisTask()
+				.setName("phone_dir_address")
+				.setInputTypes(String.class)
+				.setInputNames("s")
+				.setOutputType(String.class);
+		task.addExamples(
+				new Example().setInputs(() -> new Object[] {"<Anastasia> +48-421-674-8974 Via Quirinal Roma\n"}).setOutput("Via Quirinal Roma"),
+				new Example().setInputs(() -> new Object[] {"<P Salinger> Main Street, +1-098-512-2222, Denver\n"}).setOutput("Main Street, Denver")
+		);
+		task.addLiterals(String.class, "<Anastasia>", "<P Salinger>", "+48-421-674-8974", "+1-098-512-2222", ",", " ");
+		task.addLiterals(char.class, ',', ' ');
+		task.makeInputsImmutable();
+		tasks.add(task);
+
+		// Rot
+		task = new SynthesisTask()
+				.setName("iterative_rot_rot")
+				.setInputTypes(Integer.class, String.class)
+				.setInputNames("s")
+				.setOutputType(String.class);
+		task.addExamples(
+				new Example().setInputs(() -> new Object[] {1, "abc def"}).setOutput("fab cde"),
+				new Example().setInputs(() -> new Object[] {2, "abc def"}).setOutput("efa bcd"),
+				new Example().setInputs(() -> new Object[] {3, "abc def"}).setOutput("def abc"),
+				new Example().setInputs(() -> new Object[] {4, "abc def"}).setOutput("cde fab"),
+				new Example().setInputs(() -> new Object[] {5, "abc def"}).setOutput("bcd efa")
+		);
+		task.addLiterals(String.class, " ");
+		task.addLiterals(char.class, ' ');
 		task.makeInputsImmutable();
 		tasks.add(task);
 
